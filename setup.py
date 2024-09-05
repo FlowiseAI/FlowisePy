3c83b86c625519192b55171af81283d0cfa77801
Henry <hzj94@hotmail.com>
2024-09-05 12:55:05 +0100

    update setup py

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup, find_packages
+import os
+
+# Read the contents of your README file
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
 
 setup(
     name="flowise",
-    version="1.0.1",
+    version="1.0.2",
     description="Flowise SDK for Python to interact with the Flowise API.",
+    long_description=long_description,  # Use README.md as the long description
+    long_description_content_type='text/markdown',  # This specifies the format
     author="Henry Heng",
     author_email="support@flowiseai.com",
     url="https://github.com/FlowiseAI/FlowisePy",
